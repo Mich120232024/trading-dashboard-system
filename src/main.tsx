bdbe1045acc5880bc2597eef52bd80ac93fa02d1
MT <mt@gzcim.com>
2024-12-01 07:24:53 +0100

    001

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { FilterProvider } from "./context/FilterContext";
-import App from "./components/app/App";
+import App from "./components/app";
 import "./index.css";
 
 interface ImportMetaEnv {
